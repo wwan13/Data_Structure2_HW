826f1a839e9e7602d3b0f9d84f08d126a401bc0d
wwan13 <wwan13@naver.com>
2020-09-11 00:20:19 +0900

    file open

--- HW_1/HW_1/main.c
+++ HW_1/HW_1/main.c
@@ -1,15 +1,27 @@
-//
-//  main.c
-//  HW_1
-//
-//  Created by 김태완 on 2020/09/10.
-//  Copyright © 2020 태완. All rights reserved.
-//
-
+#define CRT_SECURE_NO_WARININGS
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+typedef struct student
+{
+    int num;
+    char name[30];
+    float grade;
+}student;
+
+int main()
+{
+    FILE *fp;
+    student tmp;
+    int count=0;
+    
+    fp = fopen("data.txt","r");
+    
+    while(!feof(fp))
+    {
+        fscanf(fp,"%d %s %f",&tmp.num,tmp.name,&tmp.grade);
+        count++;
+        //printf("%d %s %f \n",tmp.num,tmp.name,tmp.grade);
+    }
+    
+    
 }
