b3da0d3a732f9076dcba6077c1671c9fdb435270
wwan13 <wwan13@naver.com>
2020-09-10 23:56:26 +0900

    restart project

---
+++ HW_1/HW_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HW_1
+//
+//  Created by 김태완 on 2020/09/10.
+//  Copyright © 2020 태완. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
