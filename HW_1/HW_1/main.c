7b08f2560d6039cd5af24fab7dcd42b2d8450923
wwan13 <wwan13@naver.com>
2020-09-14 02:15:37 +0900

    clear HW1

--- HW_1/HW_1/main.c
+++ HW_1/HW_1/main.c
@@ -2,7 +2,7 @@
 //작성일 : 2020.09.11
 //프로그램명 : 연결리스트를 이용해 이름, 학번, 총점 순으로 정렬하는 프로그램
 
-#define CRT_SECURE_NO_WARININGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,6 +24,7 @@ typedef struct node
 
 //함수 원형 정의
 NODE* insert_data(NODE* head, STUDENT value);
+void print_type(char *string);
 void print_list(NODE *head);
 NODE* sort_by_number(NODE* head);
 NODE* sort_by_name(NODE* head);
@@ -51,15 +52,22 @@ int main()
     }
     rewind(fp);
     
+    print_type("   정렬 전    ");
     print_list(head);
-    printf("----------------------\n");
+    printf("\n");
     
-    //head = sort_by_number(head);
-    
-    //head = sort_by_name(head);
-    
+    print_type("학번 기준 정렬");
+    head = sort_by_number(head);
+    print_list(head);
+    printf("\n");
+
+    print_type("이름 기준 정렬");
+    head = sort_by_name(head);
+    print_list(head);
+    printf("\n");
+
+    print_type("총점 기준 정렬");
     head = sort_by_grade(head);
-    
     print_list(head);
 }
 
@@ -73,13 +81,26 @@ NODE* insert_data(NODE* head, STUDENT value)
     return head;
 }
 
+void print_type(char *string)
+{
+    printf("┌──────────────────────────┐\n");
+    printf("│       %8s     │\n",string);
+    printf("└──────────────────────────┘\n");
+}
+
 void print_list(NODE *head)
 // 리스트를 출력하는 함수
 {
-    for (NODE *p = head;p!=NULL;p = p->link)
+    NODE *p = NULL;
+    printf("┌──────────┬────────┬──────┐\n");
+    printf("│    학번  │   이름 │ 총점 │\n");
+    printf("├──────────┼────────┼──────┤\n");
+
+    for(p = head;p!=NULL;p=p->link)
     {
-        printf("%d %s %.2f\n",p->student.num,p->student.name,p->student.grade);
+        printf("│ %8d │ %4s │ %.2f │\n",p->student.num,p->student.name,p->student.grade);
     }
+    printf("└──────────┴────────┴──────┘\n");
 }
 
 void swap(NODE* n1, NODE* n2)
