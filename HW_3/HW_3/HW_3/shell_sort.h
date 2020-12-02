908d607bf3655ac355e925d592a649bd2e56e302
KimTaewan <wwan13@naver.com>
2020-12-02 22:29:44 +0900

    쉘정렬 문자열 기준 수정

--- HW_3/HW_3/HW_3/shell_sort.h
+++ HW_3/HW_3/HW_3/shell_sort.h
@@ -1,22 +1,24 @@
 #ifndef __SHELL_SORT__
 #define __SHELL_SORT__
 
+#include <string.h>
+
 // [gap_insertion_sort]
 // gap 의 범위만큼의 요소들에 대해 삽입 정렬 하는 알고리즘
 // @param (int data[]) -> 정렬 해야 할 데이터를 담은 리스트
 // @param (int first, int last) -> 정렬 해야할 리스트의 처음과 끝 인덱스를 담은 변수
 // @param (int gap) -> 두 인덱스의 차이
-void gap_insertion_sort( int data[],int first,int last, int gap ) {
+void gap_insertion_sort( char** data,int first,int last, int gap ) {
     
     // 반복문을 위한 변수
     int i,j;
     // 삽입할 데이터인 key값을 저장하는 변수
-    int key;
+    char* key;
     
     for( i=first+gap;i<last;i+=gap ) {
         key = data[i];
         
-        for( j=i-gap;j>=first && data[j]>key;j-=gap ) {
+        for( j=i-gap;j>=first && strcmp( data[j],key );j-=gap ) {
             data[j+gap] = data[j];
         }
         
@@ -29,7 +31,7 @@ void gap_insertion_sort( int data[],int first,int last, int gap ) {
 // 쉘 정렬 알고리즘
 // @param (int data[]) -> 정렬할 데이터를 담은 리스트
 // @param (int n) -> 데이터의 개수
-void shell_sort( int data[],int n ) {
+void shell_sort( char** data,int n ) {
     
     // 반복문을 위한 변수
     int i;
@@ -46,6 +48,10 @@ void shell_sort( int data[],int n ) {
             gap_insertion_sort(data,i,n-1,gap);
         }
     }
+    
+//    for(i=0;i<n;i++) {
+//        printf("%s " ,data[i]);
+//    }
 }
 
 #endif
