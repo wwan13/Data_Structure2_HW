d184f34d2983b2c94c050d0873c7a2489a57281a
KimTaewan <wwan13@naver.com>
2020-12-02 22:29:19 +0900

    버블정렬 문자열 기준 수정

--- HW_3/HW_3/HW_3/bubble_sort.h
+++ HW_3/HW_3/HW_3/bubble_sort.h
@@ -1,27 +1,33 @@
 #ifndef __BUBBLE_SORT__
 #define __BUBBLE_SORT__
 
+#include <string.h>
+
 #include "swap.h"
 
 // [bubble_sort]
 // 버블 정렬 알고리즘
-// @param (int data[]) -> 정렬 해야 할 데이터 리스트
+// @param (char** data) -> 정렬 해야 할 데이터 리스트
 // @param (int n) -> 데이터의 개수
-void bubble_sort( int data[], int n ) {
+void bubble_sort( char** data, int n ) {
     
     // 반복문을 위한 변수
     int i,j;
     
-    for( i=n;i>0;i-- ) {
+    for( i=n-1;i>0;i-- ) {
         
-        for( j=1;j<i;j++ ) {
+        for( j=0;j<i;j++ ) {
             
-            if( data[j]>data[j+1] ) {
+            if( strcmp(data[j],data[j+1])>0 ) {
                 swap(data[j],data[j+1]);
             }
             
         }
         
+    }
+    
+    for(i=0;i<n;i++) {
+        printf("%s ",data[i]);
     }
 }
 
