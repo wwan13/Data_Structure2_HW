6a3f0f5703c49e3ac996cd2b1110a7a034fe6f63
KimTaewan <wwan13@naver.com>
2020-12-02 22:29:28 +0900

    선택정렬 문자열 기준 수정

--- HW_3/HW_3/HW_3/insertion_sort.h
+++ HW_3/HW_3/HW_3/insertion_sort.h
@@ -1,26 +1,32 @@
 #ifndef __INSERTION_SORT__
 #define __INSERTION_SORT__
 
+#include <string.h>
+
 // [insrtion_sort]
 // 삽입정렬 알고리즘
-// @param (int list) -> 정렬할 리스트
+// @param (char** data) -> 정렬할 리스트
 // @param (int n) -> 데이터의 개수
-void insertion_sort( int data[],int n ) {
+void insertion_sort( char** data,int n ) {
     
     // 반복문을 위한 변수
     int i,j;
     // 삽입할 데이터인 key값을 저장하는 변수
-    int key;
+    char* key;
     
     for( i=0;i<n;i++ ) {
         key = data[i];
         
-        for( j=i-1;j<=0 && data[j]>key;j-- ) {
+        for( j=i-1;j>=0 && strcmp(data[j],key)>0;j-- ) {
             data[j+1] = data[j];
         }
         
         data[j+1] = key;
     }
+//
+//    for(i=0;i<n;i++) {
+//        printf("%s ",data[i]);
+//    }
 }
 
 
