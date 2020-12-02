2d4bffacf6fdc49f6f33dee28cad1295741a7909
KimTaewan <wwan13@naver.com>
2020-12-02 20:35:07 +0900

    add sort_method header

---
+++ HW_3/HW_3/HW_3/sort_methods.h
@@ -0,0 +1,10 @@
+#ifndef __SORT_METHODS__
+#define __SORT_METHODS__
+
+#include "selection_sort.h"
+#include "insertion_sort.h"
+#include "bubble_sort.h"
+#inclide "shell_sort.h"
+#include "merge_sort.h"
+
+#endif
