09b1c0b7197c2546def0bbda2ad4599fdad4ce32
KimTaewan <wwan13@naver.com>
2020-12-09 14:20:56 +0900

    10. 그래프 인접행렬, 인접 리스트

---
+++ Final_Exam/Final_Exam/Adj_mat.h
@@ -0,0 +1,57 @@
+// 인접 행렬
+
+#ifndef adj_mat_h
+#define adj_mat_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VERTICE 50
+
+typedef struct GraphType {
+    int n;
+    int adj_mat[MAX_VERTICE][MAX_VERTICE];
+}GraphType;
+
+// 그래프 초기화
+void init_mat(GraphType* g) {
+    int r,c;
+    g->n = 0;
+    for(r=0;r<MAX_VERTICE;r++) {
+        for(c=0;c<MAX_VERTICE;c++) {
+            g->adj_mat[r][c] = 0;
+        }
+    }
+}
+
+// 정접 삽입
+void insert_vertex_mat(GraphType* g, int v) {
+    if((g->n+1)>MAX_VERTICE) {
+        printf("그래프 개수 초과");
+        exit(1);
+    }
+    g->n++;
+}
+
+// 간선 삽입 연산
+void insert_edge_mat(GraphType* g, int start, int end) {
+    if(start >= g->n || end >= g->n) {
+        printf("그래프 정접 번호 오류");
+        exit(1);
+    }
+    g->adj_mat[start][end] = 1;
+    g->adj_mat[end][start] = 1;
+}
+
+// 인접 행렬 출력
+void print_adj_mat(GraphType* g) {
+    int i,j;
+    for(i=0;i<MAX_VERTICE;i++) {
+        for(j=0;j<MAX_VERTICE;j++) {
+            printf("%2d ",g->adj_mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+#endif
---
+++ Final_Exam/Final_Exam/adj_list.h
@@ -0,0 +1,68 @@
+// 인접 리스트
+
+#ifndef adj_list_h
+#define adj_list_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VERTICE 50
+
+typedef struct GraphNode {
+    int vertex;
+    struct GraphNode* link;
+}GraphNode;
+
+typedef struct GraphType {
+    int n;
+    GraphNode* adj_list[MAX_VERTICE];
+}GraphType;
+
+// 리스트 초기화
+void init(GraphType* g) {
+    int v;
+    g->n = 0;
+    for(v=0;v<MAX_VERTICE;v++) {
+        g->adj_list[v] = NULL;
+    }
+}
+
+// 정점 삽입 연산
+void insert_vertex_list(GraphType* g, int v) {
+    if((g->n+1)>MAX_VERTICE) {
+        printf("그래프 개수 초과");
+        exit(1);
+    }
+    g->n++;
+}
+
+// 간선 삽입 연산
+void insert_edge_list(GraphType* g, int u, int v) {
+    GraphNode *node;
+    
+    if(u >= g->n || v >= g->n) {
+        printf("그래프 정접 번호 오류");
+        exit(1);
+    }
+    
+    node = (GraphNode *)malloc(sizeof(GraphNode));
+    node->vertex = v;
+    node->link = g->adj_list[u];
+    g->adj_list[u] = node;
+}
+
+// 인접 리스트 출력
+void print_adj_list(GraphType *g) {
+    int i;
+    
+    for(i=0;i<g->n;i++) {
+        GraphNode *p = g->adj_list[i];
+        printf("정접 %d의 인접 리스트 ",i);
+        while (p!=NULL) {
+            printf("-> %d",p->vertex);
+            p = p->link;
+        }
+    }
+}
+
+#endif 
